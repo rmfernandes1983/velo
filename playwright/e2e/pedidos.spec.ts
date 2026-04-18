b7ac0c2a648cbf5cdb831578308bb4acbf88b8c7
Rafael Marques Fernandes <rafaelfernandes_dbcon@getnet.com.br>
2026-04-18 18:47:36 -0300

    Primeiro desafio da especializacao Automatizai

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,29 @@
 import { test, expect } from '@playwright/test';
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  //arrange
+  await page.goto('http://localhost:5173/');  
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
-  await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('VLO-XGJYNQ');
-
-  await page.getByTestId('search-order-button').click();
-  await page.getByTestId('order-result-id').click();
+  //act
   
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-XGJYNQ');
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+//assert
+/*  
+  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 });
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-XGJYNQ');
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+*/
+
+  await expect(page.getByText('VLO-XGJYNQ')).toBeVisible({ timeout: 10_000 });
+
+  await expect(page.getByText('APROVADO')).toBeVisible();
 
 });
